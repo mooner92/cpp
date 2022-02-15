aad6596601a84708f0a7e07bad7f73dadcd7eff4
Mooner92 <chlaudgjs0989@gmail.com>
2022-02-15 16:02:27 +0900

    dd

--- cpp1.cpp
+++ cpp1.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <cstring>
+
 using namespace std;
 
+double area(int r){
+    return 3.14*r*r;
+}
+
+char name[11] = {};
+
+
+int letters = strlen("kimchi");
+
 int main(){
-    cout << "inter";
+    int n=3;
+    cout << area(n) << '\n' << letters; // 글자의 개수인 6이 출력됨
+    cin >> name;
+    cout << "name is" << name;
 }
